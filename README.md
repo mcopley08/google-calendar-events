6a9945a589fca4c04c48deb3607eb5dabdde04fe
Michael <mcopley08@comcast.net>
2015-08-15 22:24:14 -0400

    added the actual code,,, LOL!

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # google-calender-events
 jQuery plugin that fetches up-to-date Google Calender information on the client-side.
+
+This was made primarily for website constractors who develop sites for their clients. It makes it so that if the client needs to update a calendar / add events to their website, they can simply update it on their Google Calendar and the change will be automatically reflected on the website. 
+
+## Getting Set-Up
+
+> The Google Calendar you draw the data from MUST be public. This makes sense because (in most cases) it will be publicly available on the website as well.
+
+1. Pull the Repository
+2. Place the ```google-calender-events.js``` or ```google-calendar-events.min.js``` into your web root.
+3. Include the file in your website by placing this in the webpage (**AFTER you've included jQuery**):
+	```
+	<script src="/path/to/google-calender-events.js"></script>
+	```
+	- or -
+	```
+	<script src="/path/to/google-calender-events.min.js"></script>
+	```
+4. Copy this code and place it underneath Step 3, but fill in the variables with your [Google API Developer Key](https://console.developers.google.com) and [Public Google Calendar ID](http://wpdocs.philderksen.com/google-calendar-events/getting-started/find-calendar-id/):
+	```
+	var mykey = '<your-google-api-key>'; 
+    var calendarid = '<public-google-calender-id>'; 
+	```
+5. Now you can use any of the methods defined in the next section to integrate with your website! :D
+
+## jQuery Calls
+
+Keep in mind that all of the returned events are ordered by their start time - so the one starting closest to the current time is returned first (at index 0), etc. Also, this doesn't return events that happen on the day the request is made.
+
+Here is how you use the plugin:
+
+### General Structure
+
+```$.grabCalendar(type, date_formatted)```
+
+### Parameters
+
+```type``` - (optional) This a string that can be either "events" or "detailedEvents". "events" will return a concise response that will be what most developers will be looking for (start, end, location, summary, description). "detailedEvents" will return a raw (& verbose) response containing a greater number of more fields. If this parameter is excluded completely, the response will give extended information for the Google Calendar including the events and general information about the calendar (timezone, name of calendar, description, etc.)
+
+```date_formatted``` - (optional) This is a boolean value that gives the date in a more friendly format if set to ```true```. For example, ```2015-08-17T06:30:00-04:00``` would become ```Monday August 17, 2015 - 6:30 AM```. If this is set to ```false``` or is excluded, the date will come in the standard ISO-8601 format (```YYYY-MM-DDThh:mmTZD```). This parameter can also be used even if no ```type``` is included in the call.
+
+## Example Calls
+
+> These are all of the supported & valid calls:
+
+```
+$.grabCalender();
+$.grabCalender("events");
+$.grabCalender("detailedEvents");
+$.grabCalender(true);
+$.grabCalender("events", true);
+$.grabCalender("detailedEvents", true);
+```
+
+Personally, I believe the ```$.grabCalender("events", true)``` will be the most practical & useful for web developers. 
+
+## Known Issues
+
+- This doesn't return events that happen on the day the request is made.
+- The Ajax call thats being made to the Google Calendar API uses the parameter ```async: false``` which gives the following warning in the developer console:
+	```
+	Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user's experience. For more help, check http://xhr.spec.whatwg.org/.
+	```
---
+++ google-calender-events.js
@@ -0,0 +1,116 @@
+
+function adjustDate(iso_date) {
+  MM = {Jan:"January", Feb:"February", Mar:"March", Apr:"April", May:"May", Jun:"June", Jul:"July", Aug:"August", Sep:"September", Oct:"October", Nov:"November", Dec:"December"};
+  DD = {Sun:"Sunday", Mon:"Monday", Tue:"Tuesday", Wed:"Wednesday", Thu:"Thursday", Fri:"Friday", Sat:"Saturday"};
+
+  proper_date = String(new Date(iso_date)).split(" ");
+
+  correct_formatting = DD[proper_date[0]] + " " + MM[proper_date[1]] + " " + proper_date[2] + ", " + proper_date[3] + " - ";
+
+  clock_time = proper_date[4].substring(0, proper_date[4].length - 3);
+
+  // the logic for printing the date in a nice fashion is a little verbose...
+  // definitely get in a pull request if you can trim this down. regex would be great <3
+
+  // if its 00, then let them know its midnight-ish
+  if (clock_time.substring(0, 2) == "00") {
+    clock_time = "12" + clock_time.substring(2, clock_time.length) + " AM";
+  }
+  // taking away the leading 0 - for the AM
+  else if (clock_time.substring(0, 1) == "0") {
+    clock_time = clock_time.substring(1, clock_time.length) + " AM";
+  }
+  // for when its 10 or 11
+  else if (Number(clock_time.substring(0, 2)) < 12) {
+    clock_time += " AM";
+  }
+  // for when its 12 (noon)
+  else if (Number(clock_time.substring(0, 2)) == 12) {
+    clock_time += " PM";
+  }
+  else if (Number(clock_time.substring(0, 2)) > 12) {
+    new_number = String(Number(clock_time.substring(0, 2)) - 12);
+    clock_time = new_number + clock_time.substring(2, clock_time.length) + " PM";
+  }
+
+  correct_formatting += clock_time;
+  return correct_formatting;
+}
+
+(function( $ ) {
+
+  $.grabCalendar = function(type, iso_format) {
+
+    console.log("Making a Google API request to return data of type: " + type);
+
+    var googleCalendarResponse;
+
+    $.ajax({
+      type: 'GET',
+      url: encodeURI('https://www.googleapis.com/calendar/v3/calendars/' + calendarid+ '/events?singleEvents=true&orderBy=startTime&key=' + mykey),
+      dataType: 'json',
+      async: false,
+      success: function (response) {
+
+        console.log("iso_format is: " + iso_format);
+
+        // checking to see if we need to change the format of dates first
+        if ((typeof iso_format != "undefined" && iso_format) || type) {
+          for (var i = 0; i < response.items.length; i++) {
+            response.items[i].end.dateTime = adjustDate(response.items[i].end.dateTime);
+            response.items[i].start.dateTime = adjustDate(response.items[i].start.dateTime);
+          }
+        }
+
+        // if theres no parameters, return the full response
+        if (typeof type == "undefined") {
+          googleCalendarResponse = response;
+        }
+
+        // 'events' just return basic information
+        else if (type === "events") {
+
+          googleCalendarResponse = [];
+
+          for (var i = 0; i < response.items.length; i++) {
+            var basicInfo = {};
+
+            basicInfo.start = response.items[i].start.dateTime;
+            basicInfo.end = response.items[i].end.dateTime;
+
+            basicInfo.summary = response.items[i].summary;
+
+            if (typeof response.items[i].description != "undefined") {
+              basicInfo.description = response.items[i].description;
+            }
+            if (typeof response.items[i].location != "undefined") {
+              basicInfo.location = response.items[i].location;
+            }
+            googleCalendarResponse.push(basicInfo);
+          }
+        }
+
+        // 'detailedEvents' returns detailed event info
+        else if (type === "detailedEvents") {
+          googleCalendarResponse = response.items;                    
+        }
+
+        // if they want the full response with pretty dates
+        else if (type) {
+          googleCalendarResponse = response;
+        }
+
+        // let them know their request wasn't valid.
+        else {
+          console.log("The request of type: " + type + " wasn't valid. Please check for possible syntax errors.");
+        }
+
+      },
+      error: function (response) {
+        //tell that an error has occurred
+        console.log("The Google Calender API failed to connect.");
+      }
+    });
+    return googleCalendarResponse;
+  };
+}( jQuery ));
